d57ac48e163bae69831f7730e8f1e4bc074bf679
Jack Welsh <48059913+JackWelsh15@users.noreply.github.com>
2019-04-08 15:28:04 +0100

    Added DBconn & xml list :card_file_box:

--- backend/DbConn.php
+++ backend/DbConn.php
@@ -1,1 +1,18 @@
+<?php
 
+// Author: Jack Welsh
+//
+// Function to get database connection.
+
+function getConnection(){
+$username = 'unn_w18020302';
+$password ='QUEYQ0YY';
+$host = 'newnumyspace.co.uk'
+$dbname ='moodwaves'
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+return $db;
+}
+
+ ?>
